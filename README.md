ac034a7bc51ab34dc5b92ce8ed5b702dbb4b47a3
jburhan <jimburhan@gmail.com>
2018-05-20 13:26:07 -0700

    initialize completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Kele
+
+Kele is a Ruby Gem API client to provide easy access to and use of the student endpoints of Bloc's API
---
+++ lib/kele.rb
@@ -0,0 +1,21 @@
+require 'httparty'
+
+class Kele
+include HTTParty
+
+  def initialize(email, password)
+    @base_url = 'https://www.bloc.io/api/v1'
+
+    response = Kele.post(
+      "#{@base_url}/sessions",
+      body: { email: email, password: password }
+    )
+
+    if response && response["auth_token"]
+      @auth_token = response["auth_token"]
+      puts "#{email} has sucessfully logged in"
+    else
+      puts "Login invalid"
+    end
+  end
+end
