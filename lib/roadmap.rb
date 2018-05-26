de5da394ca64defa7a3ae3436f7c611efe74b901
jburhan <jimburhan@gmail.com>
2018-05-26 11:58:59 -0700

    checkpoints completed

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -10,4 +10,10 @@ module Roadmap
     response = Kele.get("#{@base_url}/checkpoints/#{checkpoint_id}", headers: { "authorization" => @auth_token })
     @checkpoint = JSON.parse(response.body)
   end
+
+  def get_remaining_checkpoints(chain_id)
+    @base_url = 'https://www.bloc.io/api/v1'
+    response = Kele.get("#{@base_url}/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section", headers: { "authorization" => @auth_token })
+    @checkpoints_remaining = JSON.parse(response.body)
+  end
 end
